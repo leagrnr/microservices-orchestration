16e5e4478dfe0e7e8ce1845a866d8d4c09d073bf
AubinManceau <aubinmanceau0@gmail.com>
2025-03-10 16:38:57 +0100

    add user

--- db-init/init.sql
+++ db-init/init.sql
@@ -2,7 +2,12 @@ CREATE DATABASE IF NOT EXISTS api_db;
 
 USE api_db;
 
-CREATE TABLE IF NOT EXISTS messages (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT NOT NULL
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
 );
+
+-- Ajouter un utilisateur
+INSERT INTO users (email, password) 
+VALUES ('test@test.fr', 'test');
